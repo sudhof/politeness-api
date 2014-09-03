cdfa74624afcca362859f5f86e91e9ed5b9dac26
Moritz Sudhof <moritz.sudhof@gmail.com>
2014-09-03 10:59:55 -0700

    no sent tokenizing

--- features/bow_features.py
+++ features/bow_features.py
@@ -2,19 +2,21 @@
 import nltk
 
 def getwords(document, lasttwosents=False):
-    sents=nltk.sent_tokenize(document['text'])
-    if lasttwosents:
-      sents=sents[-2:]
-    words=[]
-    for s in sents:
-        words.extend(nltk.word_tokenize(s))
+    #sents=nltk.sent_tokenize(document['text'])
+    #if lasttwosents:
+    #  sents=sents[-2:]
+    #words=[]
+    #for s in sents:
+    #    words.extend(nltk.word_tokenize(s))
+    words = nltk.word_tokenize(document['text'])
     return words
 
 def getbigrams(document, lasttwosents=False):
-    sents=nltk.sent_tokenize(document['text'])
-    if lasttwosents:
-      sents=sents[-2:]
-    words=[]
-    for s in sents:
-        words.extend(nltk.bigrams(nltk.word_tokenize(s)))
+    #sents=nltk.sent_tokenize(document['text'])
+    #if lasttwosents:
+    #  sents=sents[-2:]
+    #words=[]
+    #for s in sents:
+    #    words.extend(nltk.bigrams(nltk.word_tokenize(s)))
+    words = nlkt.bigrams(nltk.word_tokenize(document['text']))
     return words

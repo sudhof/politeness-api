0faa42ab4192ba80498e938857a471e116f99a8e
Moritz Sudhof <moritz.sudhof@gmail.com>
2014-09-02 22:38:04 -0700

    some modeling

--- politeness_model.py
+++ politeness_model.py
@@ -11,12 +11,16 @@ from features.vectorizer import FeatureVectorizer
 clf = cPickle.load(open("linear-svm.p"))
 vectorizer = FeatureVectorizer()
 
+print "Loaded model"
 
 def score_politeness(text):
     features = vectorizer.features({"text": text})
     fv = [features[f] for f in sorted(features.iterkeys())]
+    print "Features %s" % str(fv)
     X = csr_matrix(np.asarray([fv]))
+    print "Predicting"
     y_pred = clf.predict(X)
+    print str(y_pred)
     return y_pred[0]
 
 
